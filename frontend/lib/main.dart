87d554a50fe21c9a13d66efd54dbb6fe37456ef3
joycej3 <joycej3@tcd.ie>
2023-02-05 17:46:39 +0000

    added authentication nd changed fronted lib layout

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
+import 'package:flutter_frontend/screens/login/signup.dart';
+import 'package:universal_platform/universal_platform.dart';
 import 'package:window_size/window_size.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+import 'package:flutter_frontend/screens/login/login.dart';
+import 'package:flutter_frontend/screens/report_form/myCustomForm.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (Platform.isWindows) {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  if (UniversalPlatform.isWindows) {
     setWindowTitle('Disaster Response System');
     setWindowMinSize(const Size(600, 950));
     setWindowMaxSize(const Size(600, 950));
@@ -12,77 +21,6 @@ void main() {
 
   runApp(const MyApp());
 }
-
-/////////
-// Create a Form widget.
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({super.key});
-
-  @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
-  }
-}
-
-// Create a corresponding State class.
-// This class holds data related to the form.
-class MyCustomFormState extends State<MyCustomForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a GlobalKey<FormState>,
-  // not a GlobalKey<MyCustomFormState>.
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Emergency Description: "),
-          TextFormField(
-            // The validator receives the text that the user has entered.
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter a description of your emergency';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            // The validator receives the text that the user has entered.
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your location';
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Validate returns true if the form is valid, or false otherwise.
-                if (_formKey.currentState!.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Processing Data')),
-                  );
-                }
-              },
-              child: const Text('Submit'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-//////////
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -196,7 +134,7 @@ class MyApp extends StatelessWidget {
             body: TabBarView(
               children: [
                 homePage,
-                MyCustomForm(),
+                LoginForm(),
                 Icon(Icons.report),
               ],
             ),
