2425ca1a679427145714b78224c4e56a60d4227e
joycej3 <joycej3@tcd.ie>
2023-02-10 23:57:38 +0000

    added a signout button and change frontend structure

---
+++ frontend/lib/services/authentication.dart
@@ -0,0 +1,76 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:async';
+// import 'package:firebase_messaging/firebase_messaging.dart';
+
+import 'package:flutter_frontend/main.dart';
+import 'package:flutter_frontend/screens/sensitive/workerhome.dart';
+import 'package:flutter_frontend/screens/home.dart';
+
+class AuthenticationHelper {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  get user => _auth.currentUser;
+  String uri = 'http://localhost:8080/firebase_get';
+
+  handleAuth() {
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            print("has_Data");
+            return WorkerHome();
+          } else {
+            print("has no Data");
+            return Home();
+          }
+        });
+  }
+
+  //SIGN IN METHODJ
+  Future<String?> signIn(
+      {required String email, required String password}) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  Future<String> extractTokenAndAccessSecureResource() async {
+    var token = await extractToken();
+    return await accessSecureResource(token);
+  }
+
+  Future<String> extractToken() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    User? user = firebaseAuth.currentUser;
+    if (user != null) {
+      String? idToken = await user.getIdToken();
+      return idToken;
+    }
+    return "";
+  }
+
+  Future<String> accessSecureResource(token) async {
+    Map<String, String> headers = {
+      "Content-type": "application/json",
+      "Authorization": "Bearer " + token
+    };
+    print("Token " + token);
+    Response response = await get(Uri.parse(uri), headers: headers);
+    int statusCode = response.statusCode;
+    if (statusCode != 200) {
+      return "Could not get input from server";
+    }
+    return response.body.toString();
+  }
+
+  //SIGN OUT METHOD
+  signOut() {
+    print("sign out");
+    FirebaseAuth.instance.signOut();
+  }
+}
--- frontend/test/server_skeleton_test.dart
+++ frontend/test/server_skeleton_test.dart
@@ -6,32 +6,29 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_frontend/api.dart';
-import 'package:flutter_frontend/api.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_frontend/ServerSkeleton.dart';
+import 'package:flutter_frontend/server_skeleton.dart';
 
-import 'ServerSkeleton_test.mocks.dart';
+import 'server_skeleton_test.mocks.dart';
 
 const String SERVER_URL = 'http://localhost:8080/firebase_get';
 
-
 @GenerateMocks([ApiHandler, http.Response, http.Client])
 void main() {
-
-  test(' GIVEN a http client WHEN the http call completes successfully THEN returns a DisasterResponse', () async {
+  test(
+      ' GIVEN a http client WHEN the http call completes successfully THEN returns a DisasterResponse',
+      () async {
     //GIVEN
     final apiHandler = MockApiHandler();
     // Use Mockito to return a successful response when it calls the
     // provided http.Client.
     //WHEN
-    when(apiHandler
-        .callApi(any))
-        .thenAnswer((_) async =>
-        http.Response('{"DisasterType": "Hurricane", "DisasterTime": "12:06"}', 200));
+    when(apiHandler.callApi(any)).thenAnswer((_) async => http.Response(
+        '{"DisasterType": "Hurricane", "DisasterTime": "12:06"}', 200));
 
     //THEN
     expect(await fetchAlbum(apiHandler), isA<DisasterResponse>());
@@ -39,20 +36,17 @@ void main() {
 
   test("GIVEN a http client WHEN 404 is returned THEN throws exception", () {
     //GIVEN
-     final apiHandler = MockApiHandler();
+    final apiHandler = MockApiHandler();
     String mockResponse = 'Not Found';
 
     //WHEN
-    when(apiHandler
-        .callApi(any))
-        .thenAnswer((_) async =>
-        http.Response(mockResponse, 404));
+    when(apiHandler.callApi(any))
+        .thenAnswer((_) async => http.Response(mockResponse, 404));
 
     expect(fetchAlbum(apiHandler), throwsException);
   });
 
-  test("Empty test", ()
-  {
+  test("Empty test", () {
     //GIVEN
     int dummyVar = 0;
     //WHEN
