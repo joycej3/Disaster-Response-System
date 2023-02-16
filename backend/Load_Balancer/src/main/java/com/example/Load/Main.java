482fa27f7b42593a3d60e5bcc6bcb2f22e8812f8
GH-James <78970503+GH-James@users.noreply.github.com>
2023-02-16 17:25:02 +0000

    Pass backend requests through load balancer

--- backend/Load_Balancer/src/main/java/com/example/Load/Main.java
+++ backend/Load_Balancer/src/main/java/com/example/Load/Main.java
@@ -1,5 +1,6 @@
 package com.example.Load;
 
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import weka.core.converters.JSONLoader;
+
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
 
 
 @RestController
@@ -22,8 +36,25 @@ import java.io.InputStream;
 public class Main {
 
 	@GetMapping("/greeting")
-	public String  greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return "temp";
+	public HashMap greeting(@RequestParam(value = "name", defaultValue = "World") String name) throws IOException, InterruptedException {
+		
+		// create a client
+		var client = HttpClient.newHttpClient();
+		// create a request
+		var request = HttpRequest.newBuilder(
+			URI.create("http://localhost:8081/greeting"))
+		.header("accept", "application/json")
+		.build();
+
+		// use the client to send the request
+		var response = client.send(request, BodyHandlers.ofString());
+
+		JsonObject jsonObject = new JsonParser().parse(response.body()).getAsJsonObject();
+		System.out.println(jsonObject);
+		HashMap<String, Object> hashMap = new Gson().fromJson(jsonObject, HashMap.class);
+		System.out.println(hashMap);
+		return hashMap;
+		
 	}
 
 }
