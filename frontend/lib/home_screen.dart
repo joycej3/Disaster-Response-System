1d8ea13440372b6e3ed66f4be4d68924f0eb3339
KSK-max <67270061+KSK-max@users.noreply.github.com>
2023-02-28 11:42:46 +0000

    Get loaction

--- frontend/lib/home_screen.dart
+++ frontend/lib/home_screen.dart
@@ -1,56 +1,104 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomePageState extends State<HomePage> {
+  Completer<GoogleMapController> _controller = Completer();
+// on below line we have specified camera position
+  static final CameraPosition _kGoogle = const CameraPosition(
+    target: LatLng(20.42796133580664, 80.885749655962),
+    zoom: 14.4746,
+  );
 
+// on below line we have created the list of markers
+  final List<Marker> _markers = <Marker>[
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(20.42796133580664, 75.885749655962),
+        infoWindow: InfoWindow(
+          title: 'My Position',
+        )
+    ),
+  ];
 
-  @override
-  void initState(){
-    getData();
-    super.initState();
+// created method for getting user current location
+  Future<Position> getUserCurrentLocation() async {
+    await Geolocator.requestPermission().then((value){
+    }).onError((error, stackTrace) async {
+      await Geolocator.requestPermission();
+      print("ERROR"+error.toString());
+    });
+    return await Geolocator.getCurrentPosition();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Http Get Request'),
+        backgroundColor: Color(0xFF0F9D58),
+        // on below line we have given title of app
+        title: Text("GFG"),
       ),
-      body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: ExpansionTile(
-              title: Text('HTTP'),
-              children: [Text('Testing')],
-            ),
-          );
+      body: Container(
+        child: SafeArea(
+          // on below line creating google maps
+          child: GoogleMap(
+            // on below line setting camera position
+            initialCameraPosition: _kGoogle,
+            // on below line we are setting markers on the map
+            markers: Set<Marker>.of(_markers),
+            // on below line specifying map type.
+            mapType: MapType.normal,
+            // on below line setting user location enabled.
+            myLocationEnabled: true,
+            // on below line setting compass enabled.
+            compassEnabled: true,
+            // on below line specifying controller on map complete.
+            onMapCreated: (GoogleMapController controller){
+              _controller.complete(controller);
+            },
+          ),
+        ),
+      ),
+      // on pressing floating action button the camera will take to user current location
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async{
+          getUserCurrentLocation().then((value) async {
+            print(value.latitude.toString() +" "+value.longitude.toString());
+
+            // marker added for current users location
+            _markers.add(
+                Marker(
+                  markerId: MarkerId("2"),
+                  position: LatLng(value.latitude, value.longitude),
+                  infoWindow: InfoWindow(
+                    title: 'My Current Location',
+                  ),
+                )
+            );
+
+            // specified current users location
+            CameraPosition cameraPosition = new CameraPosition(
+              target: LatLng(value.latitude, value.longitude),
+              zoom: 14,
+            );
+
+            final GoogleMapController controller = await _controller.future;
+            controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+            setState(() {
+            });
+          });
         },
-        itemCount: 1,
+        child: Icon(Icons.local_activity),
       ),
     );
-  }
-
-  Future<void> getData() async{
-    var urlString = 'https://api.nationaltransport.ie/gtfsr/v1?format=json';
-    var uri = Uri.parse(urlString);
-    var response = await http.get(
-      uri,
-      headers: {
-        'x-api-key': '8acd9db0fc384b34945d08b5f631ac20',
-      },
-    );
-
-    if (response.statusCode == 200) {
-      var data = json.decode(response.body);
-      print(data);
-    } else {
-      print('Request failed with status: ${response.statusCode}');
-    }
   }
 }
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,14 +1,28 @@
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+// This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      // on below line we are specifying title of our app
+      title: 'GFG',
+      // on below line we are hiding debug banner
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // on below line we are specifying theme
+        primarySwatch: Colors.green,
+      ),
+      // First screen of our app
+      home: const HomePage(),
     );
   }
 }
