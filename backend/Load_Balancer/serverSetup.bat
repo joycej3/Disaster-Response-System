fbf12b13fcff4c6a08dd398617b8a3f8b89a0cc4
GH-James <78970503+GH-James@users.noreply.github.com>
2023-03-01 11:09:31 +0000

    Updated serverSetup.dart to reflect changes to load balancing

--- backend/Load_Balancer/serverSetup.bat
+++ backend/Load_Balancer/serverSetup.bat
@@ -1,13 +1,20 @@
 @echo off
 dir
-start /B /wait cmd /c mvnw clean install
+if "%~1"=="server" (start /B /wait cmd /c mvnw clean install)
+if "%~1"=="server" (start /B /wait cmd /c python ip_discovery.py)
+if "%~1"=="server" (start /B cmd /c java -jar target/rest-service-complete-0.0.1-SNAPSHOT.jar) else (echo "not running server")
+
 
 cd ../../frontend
 dir
 start /B /wait cmd /c flutter build web
+start /B /wait cmd /c docker build -t aswe/frontend .
+start /B /wait cmd /c docker run -d -p 80:80 aswe/frontend
+
+
  
 cd ../backend/Api_Backend
 dir
 start /B /wait cmd /c mvnw clean install 
-cd ../Load_Balancer
-start /B /wait cmd /c docker-compose up --build
+start /B /wait cmd /c docker build -t aswe/backend .
+start /B /wait cmd /c docker run -d -p 8081:8080 aswe/backend
