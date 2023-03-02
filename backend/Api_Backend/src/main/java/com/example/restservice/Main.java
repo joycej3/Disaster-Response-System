0f54e5e27e064dd0fb7e0b9f02dfc401d5b542fd
GH-James <78970503+GH-James@users.noreply.github.com>
2023-03-02 16:24:17 +0000

    Removed register from java in api_backend main Now handled by scripts in previous commit

--- backend/Api_Backend/src/main/java/com/example/restservice/Main.java
+++ backend/Api_Backend/src/main/java/com/example/restservice/Main.java
@@ -33,8 +33,6 @@ public class Main {
 
 	@Autowired
 	public Main(FirebaseDatabase getDatabase) throws java.io.FileNotFoundException, java.io.IOException {
-		registerWithLoadBal(getDatabase);
-
 		disasterRef = getDatabase.getReference("test/disasters");
 		
 		// Attach a listener to read the data at our posts reference
@@ -99,54 +97,5 @@ public class Main {
 	// 	return new EmergencyRecord(recentEmergency.type, recentEmergency.time);
 	// }
 	// FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
-
-
-	private void registerWithLoadBal(FirebaseDatabase database){
-		DatabaseReference ipRef = database.getReference("ip");
-		ipRef.addChildEventListener(new ChildEventListener() {
-			@Override
-			public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
-				System.out.println("ipRef got data");
-				String loadBalIp = dataSnapshot.getValue(String.class);
-				System.out.println("got through datasnapshot.getvalue");
-				System.out.println("Got ip from firebase: " + loadBalIp);
-
-				String localIp = loadBalIp;
-				
-				HttpClient client = HttpClient.newHttpClient();
-				HttpRequest request = HttpRequest.newBuilder(URI.create("http://" + loadBalIp + ":8080/register"))
-					.header("content-type", "application/json")
-					.POST(HttpRequest.BodyPublishers.ofString("{\"type\":\"backend\",\"ip\":\"" + localIp + "\"}"))
-					.build();
-			
-				try {
-					client.send(request, BodyHandlers.ofString());
-				} catch (IOException | InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-			}
-		  
-			@Override
-			public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {
-				System.out.println("changed");
-			}
-		  
-			@Override
-			public void onChildRemoved(DataSnapshot dataSnapshot) {
-				System.out.println("removed");
-			}
-		  
-			@Override
-			public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {
-				System.out.println("moved");
-			}
-		  
-			@Override
-			public void onCancelled(DatabaseError databaseError) {
-				System.out.println("cancelled");
-			}
-		  });
 	}
 }
