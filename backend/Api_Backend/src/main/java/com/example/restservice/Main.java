13f62314d080fb503226893884056e0e6decc6fc
GH-James <78970503+GH-James@users.noreply.github.com>
2023-03-02 17:38:05 +0000

    Fixed errors caused by merge

--- backend/Api_Backend/src/main/java/com/example/restservice/Main.java
+++ backend/Api_Backend/src/main/java/com/example/restservice/Main.java
@@ -105,5 +105,5 @@ public class Main {
 	// 	return new EmergencyRecord(recentEmergency.type, recentEmergency.time);
 	// }
 	// FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
-	}
+	//}
 }
