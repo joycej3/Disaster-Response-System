140bf8eb767f58cac18f6982f417e5b3db38bc19
Dolapo Adedokun <adedokun@mit.edu>
2023-03-14 11:50:49 +0000

    added convex hull, report clasifier and report aggregator skeleton code

---
+++ backend/Api_Backend/src/main/java/com/example/restservice/ConvexHull.java
@@ -0,0 +1,88 @@
+package com.example.restservice;
+
+import java.util.*;
+
+public class ConvexHull {
+    
+    private static class Point implements Comparable<Point> {
+        double lat, lon;
+        
+        public Point(double lat, double lon) {
+            this.lat = lat;
+            this.lon = lon;
+        }
+        
+        public int compareTo(Point other) {
+            if (this.lat < other.lat)
+                return -1;
+            if (this.lat > other.lat)
+                return 1;
+            if (this.lon < other.lon)
+                return -1;
+            if (this.lon > other.lon)
+                return 1;
+            return 0;
+        }
+    }
+    
+    private static double orientation(Point p, Point q, Point r) {
+        double val = (q.lon - p.lon) * (r.lat - q.lat) - (q.lat - p.lat) * (r.lon - q.lon);
+        if (val == 0)
+            return 0;
+        return (val > 0) ? 1 : 2;
+    }
+    
+    private static ArrayList<Point> convexHull(Point[] points) {
+        int n = points.length;
+        ArrayList<Point> hull = new ArrayList<Point>();
+        
+        // Find the southernmost point
+        int min = 0;
+        for (int i = 1; i < n; i++) {
+            if (points[i].compareTo(points[min]) < 0) {
+                min = i;
+            }
+        }
+        final Integer innerMin = Integer.valueOf(min);
+        // Sort points by polar angle with respect to min point
+        Arrays.sort(points, min + 1, n, new Comparator<Point>() {
+            public int compare(Point p, Point q) {
+                double o = orientation(points[innerMin], p, q);
+                if (o == 0) {
+                    return (int)((p.lat + p.lon) - (q.lat + q.lon));
+                }
+                return (o == 2) ? -1 : 1;
+            }
+        });
+        
+        // Add the first two points to the hull
+        hull.add(points[min]);
+        hull.add(points[(min + 1) % n]);
+        
+        // Add remaining points to the hull
+        for (int i = 2; i < n; i++) {
+            Point p = points[i];
+            while (hull.size() > 1 && orientation(hull.get(hull.size() - 2), hull.get(hull.size() - 1), p) != 2) {
+                hull.remove(hull.size() - 1);
+            }
+            hull.add(p);
+        }
+        
+        return hull;
+    }
+    
+    protected static Integer Integer(int min) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Point[] points = { new Point(37.7850, -122.4089), new Point(37.7766, -122.3958), new Point(37.7801, -122.4016),
+                           new Point(37.7749, -122.4312), new Point(37.7933, -122.4157), new Point(37.7869, -122.4111),
+                           new Point(37.7885, -122.3985), new Point(37.7949, -122.4081), new Point(37.7983, -122.4192) };
+        ArrayList<Point> hull = convexHull(points);
+        System.out.println("Convex hull:");
+        for (Point p : hull) {
+            System.out.println("(" + p.lat + ", " + p.lon + ")");
+        }
+    }
+}
---
+++ backend/Api_Backend/src/main/java/com/example/restservice/ReportAggregator.java
@@ -0,0 +1,34 @@
+package com.example.restservice;
+
+
+public class ReportAggregator {
+
+    // takes all reports under a disaster ID and aggregates them to pass to ML model
+    // maybe create an aggregated report class to pass to ML model
+    public static void aggregateReports(){
+
+    }
+
+    private int aggregatePopulation(){
+        //take max over all the populations 
+        return 0;
+    }
+    private int aggregateKnownInjury(){
+        // take max over all the injuries 
+        return 0;
+    }
+
+    private int aggregateIncidentType(){
+        // take mode over all report values 
+        return 0;
+    }
+
+    private int aggregateArea(){
+        //take all lat long of the reports
+        //run convext hull to get list of vertices of bounding polygon of points
+        //run shoelace formula with these vertice to get area 
+        return 0;
+    }
+
+    
+}
