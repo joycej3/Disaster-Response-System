df5cd3178f6cb1ef86115b19cac316ec68ae1bd8
Cooperal <cooperal@tcd.ie>
2023-03-15 16:01:51 +0000

    a few more LoadBalancer tests (leaving Http mocking alone for now

--- backend/Load_Balancer/src/test/java/com/example/Load/MainTests.java
+++ backend/Load_Balancer/src/test/java/com/example/Load/MainTests.java
@@ -1,18 +1,28 @@
 package com.example.Load;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,6 +35,12 @@ public class MainTests {
 	
 	@Mock
 	HttpServletRequest servletRequest;
+
+	@Mock
+	HttpClient httpClient;
+
+	@Mock 
+	HttpResponse<Object> httpResponse;
 
 	Main main = new Main();
 
@@ -90,6 +106,30 @@ public class MainTests {
 		assertEquals("failed", response.getBody());
 	}
 	
+	@Test
+	public void getNextIpEmptyTest(){
+		//GIVEN
+		main.serverTypeToIpList.replace("backend", new ArrayList<>());
+
+		//WHEN
+        var nextIpIndex = ReflectionTestUtils.invokeMethod(main, "getNextIp", "backend");
+
+		//THEN
+		assertEquals("", nextIpIndex);
+	}
+
+	@Test
+	public void getNextIpLastHeartbeatTest(){
+		//GIVEN
+		main.ipToLastHeartbeat.remove("1.1.1.1");
+
+		//WHEN
+        var nextIpIndex = ReflectionTestUtils.invokeMethod(main, "getNextIp", "backend");
+
+		//THEN
+		assertEquals("", nextIpIndex);
+	}
+
 	@Test
 	public void getNextIpFirstRunTest(){
 		//GIVEN
@@ -163,6 +203,56 @@ public class MainTests {
 		backendActiveIps.add("1.1.1.1");
 		backendActiveIps.add("1.1.1.2");
 		assertEquals(backendActiveIps, main.serverTypeToIpList.get("backend"));
+	}
+
+	@Test
+	public void backendPostEmptyIpListTest(){
+		//GIVEN
+		main.serverTypeToIpList.replace("backend", new ArrayList<>());
+		var retResponse = new ResponseEntity<>("", HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		//WHEN
+		try{
+        var response = main.backendPost(servletRequest);
+		System.out.println(response);
+
+		//THEN
+		assertEquals(retResponse, response);
+		} catch (Exception e){assertTrue(false);}
+	}
+
+	@Test
+	public void backendGetEmptyIpListTest(){
+		//GIVEN
+		main.serverTypeToIpList.replace("backend", new ArrayList<>());
+		var retResponse = new ResponseEntity<>(new HashMap<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+		Map <String, String> params = new HashMap<>();
+		params.put("test", "test");
+
+		//WHEN
+		try{
+        var response = main.backendGet(params, servletRequest);
+		System.out.println(response);
+
+		//THEN
+		assertEquals(retResponse, response);
+		} catch (Exception e){assertTrue(false);}
+	}
+
+	@Test
+	public void frontendGetEmptyIpListTest(){
+		//GIVEN
+		main.serverTypeToIpList.replace("frontend", new ArrayList<>());
+		var retResponse = new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+
+		//WHEN
+		try{
+        var response = main.frontend(servletRequest);
+		System.out.println(response);
+
+		//THEN
+		assertEquals(retResponse, response);
+		} catch (Exception e){assertTrue(false);}
 	}
 
 }
