d8445632b4ffbe703d2e2e90b6aed4962da1b03b
unknown <whitep5@tcd.ie>
2023-03-21 12:49:38 +0000

    Coordinator Page updates

---
+++ frontend/lib/screens/sensitive/coordinator/suggestions.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_frontend/screens/sensitive/coordinator/coordinator_map.dart';
+import 'package:flutter_frontend/screens/sensitive/coordinator/big_red_button.dart';
+import 'package:flutter_frontend/services/api.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class CoordinatorSuggestions extends StatefulWidget {
+  CoordinatorSuggestions({Key? key}) : super(key: key);
+
+  @override
+  SuggestionDropdown createState() => SuggestionDropdown();
+}
+
+class SuggestionDropdown extends State<CoordinatorSuggestions> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        // home: Theme(
+        //   data: ThemeData.from(
+        //     colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue),
+        //   ),
+        //   child:
+        // ),
+        home: DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Coordinator Page'),
+          bottom: const TabBar(
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorWeight: 10,
+            tabs: [
+              Tab(icon: Icon(Icons.home)),
+              Tab(icon: Icon(Icons.format_list_bulleted)),
+              Tab(icon: Icon(Icons.error))
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Row(
+              children: [
+                Text(
+                  'Options',
+                  style: TextStyle(fontSize: 70),
+                ),
+                Expanded(child: CoordinatorMap()),
+              ],
+            ),
+            Expanded(child: CoordinatorMap()),
+            RedButton(),
+          ],
+        ),
+      ),
+    ));
+  }
+}
