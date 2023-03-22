9beb26c08588f1a746df07b4df11c0919ff56261
GH-James <78970503+GH-James@users.noreply.github.com>
2023-03-22 12:19:26 +0000

    improved load balancer logging + added cross origin to api backend

--- backend/Api_Backend/src/main/java/com/example/restservice/Main.java
+++ backend/Api_Backend/src/main/java/com/example/restservice/Main.java
@@ -21,6 +21,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,6 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.firebase.database.*;
 
+@CrossOrigin
 @RestController
 @Component
 public class Main {
