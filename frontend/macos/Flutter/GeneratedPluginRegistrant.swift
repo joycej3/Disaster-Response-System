cb88523e0940a4953aad0e2f0bc8fb845f3c0778
unknown <whitep5@tcd.ie>
2023-03-29 12:28:11 +0100

    Changes to frontend

--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import firebase_auth
 import firebase_core
 import firebase_database
 import firebase_ml_model_downloader
+import location
 import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -16,5 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FirebaseModelDownloaderPlugin.register(with: registry.registrar(forPlugin: "FirebaseModelDownloaderPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
