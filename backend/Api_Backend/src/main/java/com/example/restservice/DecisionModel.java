a27b54d4a0eb6b001e81ab1a752a8370e1d6a157
ofarrero <ro.ofarrell1@gmail.com>
2023-03-29 12:33:33 +0100

    Add working Model

--- backend/Api_Backend/src/main/java/com/example/restservice/DecisionModel.java
+++ backend/Api_Backend/src/main/java/com/example/restservice/DecisionModel.java
@@ -1,31 +1,47 @@
 package com.example.restservice;
 
 import org.pmml4s.model.Model;
-import org.apache.commons.math3.util.Pair;
-
-import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
 
 public class DecisionModel {
 
-    private final Model model = Model.fromFile(Main.class.getClassLoader().getResource("regression_model.pmml").getFile());
-
-    public static void main(String[] args){
-      test();
+    private final static Model ambo_model = Model.fromFile("Api_Backend/src/main/java/com/example/restservice/regression_model_ambos.pmml");
+    private final static Model fire_engine_model = Model.fromFile("Api_Backend/src/main/java/com/example/restservice/regression_model_fire_engine.pmml");
+    private final static Model fire_workers_model = Model.fromFile("Api_Backend/src/main/java/com/example/restservice/regression_model_fire_workers.pmml");
+    private final static Model medics_model = Model.fromFile("Api_Backend/src/main/java/com/example/restservice/regression_model_medics.pmml");
+    private final static Model traffic_police_model = Model.fromFile("Api_Backend/src/main/java/com/example/restservice/regression_model_traffic_police.pmml");
+    public static void main(String[] args){    
+        test();
     }
 
-    public Object getRegressionValue(Map<String, Double> values) {
-        Object[] valuesMap = Arrays.stream(model.inputNames())
+    public static Map<String, Integer> getSuggestions(Map<String, Double> values) {
+        Object[] valuesMap = Arrays.stream(ambo_model.inputNames())
                 .map(values::get)
                 .toArray();
 
-        Object[] result = model.predict(valuesMap);
-        return result;
+        Object[] ambo_result = ambo_model.predict(valuesMap);
+        Object[] fe_result = fire_engine_model.predict(valuesMap);
+        Object[] fw_result = fire_workers_model.predict(valuesMap);
+        Object[] medic_result = medics_model.predict(valuesMap);
+        Object[] tp_result = traffic_police_model.predict(valuesMap);
+
+        Double ambo_d = (Double) ambo_result[0];
+        Double fe_d = (Double) fe_result[0];
+        Double fw_d = (Double) fw_result[0];
+        Double medic_d = (Double) medic_result[0];
+        Double tp_d = (Double) tp_result[0];
+
+        Map<String, Integer> suggestions = new HashMap<>();
+        suggestions.put("Ambulances", ambo_d.intValue());
+        suggestions.put("Paramedics", medic_d.intValue());
+        suggestions.put("Fire Trucks", fe_d.intValue());
+        suggestions.put("Police", tp_d.intValue());
+        suggestions.put("Fire-Fighters", fw_d.intValue());
+
+        return suggestions;
     }
 
     public static void test() {
-        System.out.println("Test");
         Map<String, Double> values = new HashMap<>();
         values.put("known_injury", 0d);
         values.put("incident_type_code", 1d);
@@ -40,7 +56,7 @@ public class DecisionModel {
         values.put("weather_rain", 0d);
         values.put("weather_sunshine", 0d);
 
-        double predicted = main.getRegressionValue(values);
+        Map<String, Integer> predicted = getSuggestions(values);
         System.out.println(predicted);
     }
 }
