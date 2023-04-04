78329ebc783982bd1d0af1e06a595aeef1175efc
GH-James <78970503+GH-James@users.noreply.github.com>
2023-04-04 10:43:01 +0100

    Added get suggestion api endpoint for testing

--- backend/Api_Backend/src/main/java/com/example/restservice/controllers/WorkerController.java
+++ backend/Api_Backend/src/main/java/com/example/restservice/controllers/WorkerController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.restservice.security.roles.IsWorker;
@@ -23,5 +24,17 @@ public class WorkerController {
 		returnValue.put("value", "You have accessed Worker only data from spring boot");
 		return returnValue;
 	}
-	
+
+	@GetMapping("get_suggestion")
+	@IsWorker
+	public HashMap getSuggestion(@RequestParam String id) {
+		HashMap<String, String> returnValue = new HashMap<>();
+		returnValue.put("ambulances", "2");
+		returnValue.put("paramedics", "3");
+		returnValue.put("fire_engines", "2");
+		returnValue.put("police", "22");
+		returnValue.put("fire_fighters", "7");
+		return returnValue;
+	}
+
 }
