64e0d2000c6ac0feb606e6e079892db9608734c7
GH-James <78970503+GH-James@users.noreply.github.com>
2023-04-05 12:03:27 +0100

    Fixed database to parse ongoing disaster correctly

--- backend/Api_Backend/src/main/java/com/example/restservice/Database.java
+++ backend/Api_Backend/src/main/java/com/example/restservice/Database.java
@@ -13,7 +13,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class Database {
     private FirebaseDatabase database;
     private String suggestionRefPath = "something/suggestions";
-    private String ongoingDisastersRefPath = "ReportTable/Uncategorised";
+    private String ongoingDisastersRefPath = "ReportTable/Categorised/Ongoing";
     private String decisionsRefPath = "Decisions";
     private String userRefPath = "users";
 	private String emergencyRefPath = "ReportTable/Uncategorised";
@@ -93,20 +93,31 @@ public class Database {
 		ongoingDisastersRef.addChildEventListener(new ChildEventListener() {
 			@Override
 			public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
-				HashMap<String, Object> disaster = dataSnapshot.getValue(HashMap.class);
+				System.out.println("Ongoing disaster added");
+				HashMap<String, Object> disaster = new HashMap<>();
+				for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+					String subKey = snapshot.getKey();
+					if(subKey != "Reports"){
+						String value = snapshot.getValue(Object.class).toString();
+						disaster.put(subKey, value);
+						System.out.println(subKey + ": " + value);
+					}
+				}
                 String key = dataSnapshot.getKey();
+				System.out.println("ongoing disaster added: " + key);
 				disasterIdToOngoingDisaster.put(key, disaster);
+				System.out.println("ongoing disaster added succesfully: " + key);
 				
 				HashMap<String, Double> parsed_disaster = new HashMap<>();
-				parsed_disaster.put("known_injury", Double.valueOf(disasterIdToOngoingDisaster.get("KnownInjury").toString()));
-				parsed_disaster.put("incident_type_code", Double.valueOf(disasterIdToOngoingDisaster.get("IncidentType").toString()));
-				parsed_disaster.put("area_size", Double.valueOf(disasterIdToOngoingDisaster.get("Area").toString()));
-				parsed_disaster.put("first_report", Double.valueOf(disasterIdToOngoingDisaster.get("FirstReported").toString()));
+				parsed_disaster.put("known_injury", Double.valueOf(disaster.get("KnownInjury").toString()));
+				parsed_disaster.put("incident_type_code", Double.valueOf(disaster.get("IncidentType").toString()));
+				parsed_disaster.put("area_size", Double.valueOf(disaster.get("Area").toString()));
+				parsed_disaster.put("first_report", Double.valueOf(disaster.get("FirstReported").toString()));
 				parsed_disaster.put("location_Dn Laoghaire-Rathdown", 0d);
 				parsed_disaster.put("location_Dublin City", 0d);
 				parsed_disaster.put("location_Fingal", 0d);
 				parsed_disaster.put("location_South Dublin", 0d);
-				String neighborhood = disasterIdToOngoingDisaster.get("Location").toString();
+				String neighborhood = disaster.get("Location").toString();
 				if(neighborhood == "FN"){
 					parsed_disaster.put("location_Fingal", 1d);
 				} else if (neighborhood == "DLR"){
@@ -122,7 +133,7 @@ public class Database {
 				parsed_disaster.put("weather_sunshine", 0d);
 
 				disasterIdToOngoingDisasterModel.put(key, parsed_disaster);
-				System.out.println("added ongoing disaster: " + key);
+				System.out.println("added disaster for model parsing: " + key);
 			}
 		  
 			@Override
