cd92be2be737f09fd533d6ae281958a0cbef8b14
PW <whitep5@tcd.ie>
2023-04-11 11:23:26 +0100

    Bigger...redder...button :)

--- frontend/lib/screens/sensitive/coordinator/big_red_button.dart
+++ frontend/lib/screens/sensitive/coordinator/big_red_button.dart
@@ -3,7 +3,6 @@ import 'package:flutter_frontend/screens/login.dart';
 import 'package:flutter_frontend/screens/custom_form.dart';
 import 'package:flutter_frontend/screens/map.dart';
 import 'package:http/http.dart';
-
 import '../../../services/api.dart';
 import '../../../services/authentication.dart';
 
@@ -19,25 +18,54 @@ class DrawRedButton extends State<RedButton> {
   Widget build(BuildContext context) {
     AuthenticationHelper authenticationHelper = AuthenticationHelper();
     return MaterialApp(
-      home: Scaffold(
-        body: SizedBox(
-            width: double.infinity,
-            height: double.infinity,
-            child: Align(
-                alignment: Alignment.center,
-                child: ElevatedButton(
-                  onPressed: () => pushButton(authenticationHelper),
-                  child: Container(
-                    width: 300.0,
-                    height: 300.0,
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      shape: BoxShape.circle,
-                    ),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 40,
+                  child: Text(
+                    "End Disaster Response by clicking X button below: ",
+                    style: TextStyle(
+                        fontSize: 25,
+                        backgroundColor: Colors.red,
+                        color: Colors.white),
                   ),
-                ))),
-      ),
-    );
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                RawMaterialButton(
+                  onPressed: () => pushButton(authenticationHelper),
+                  elevation: 2.0,
+                  fillColor: Colors.red,
+                  padding: EdgeInsets.all(15.0),
+                  shape: CircleBorder(),
+                  child: Icon(
+                    Icons.cancel_sharp,
+                    color: Colors.white,
+                    size: MediaQuery.of(context).size.height / 2.5,
+                  ),
+                ),
+                SizedBox(height: 75),
+                ElevatedButton(
+                  onPressed: () => AuthenticationHelper().signOut(),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.red),
+                  child: Text('Press this button to Logout',
+                      style: TextStyle(fontSize: 28)),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 
   Future<void> pushButton(AuthenticationHelper authenticationHelper) async {
