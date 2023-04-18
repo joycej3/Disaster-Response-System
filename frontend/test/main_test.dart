c255976368905dbafd238a28aaf53ccc759a2d0e
PW <whitep5@tcd.ie>
2023-04-18 13:57:41 +0100

    changes to tests

--- frontend/test/main_test.dart
+++ frontend/test/main_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_frontend/screens/sensitive/coordinator/coordinator_options.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_frontend/screens/home.dart';
 import 'package:flutter_frontend/screens/sensitive/coordinator/coordinator.dart';
@@ -9,35 +10,6 @@ import 'package:flutter_frontend/screens/custom_form.dart';
 import 'package:flutter_frontend/screens/login.dart';
 
 void main() {
-  ////////   COORDINATOR PAGES TESTS ---- START //////
-  testWidgets('Coordinator Landing Page Test: ', (tester) async {
-    // Tests the blue landing page.
-    print('Coordinator Landing Page testing in progress...');
-    await tester.pumpWidget(CoordinatorHome());
-    //if a certain phrase can be found on the page, test passes
-    final textFinder = find.text('DRS');
-    expect(textFinder, findsOneWidget);
-  });
-  testWidgets('Big Red Button Page Test: ', (tester) async {
-    // Tests the big red button page.
-    print('Big Red Button Page testing in progress...');
-    await tester.pumpWidget(RedButton());
-    //if a certain phrase can be found on the page, test passes
-    final textFinder = find.text('Press this button to Logout');
-    expect(textFinder, findsOneWidget);
-  });
-  testWidgets('Coordinator Page Test: ', (tester) async {
-    // Tests the coordinator info page.
-    print('Coordinator Page testing in progress...');
-    await tester.pumpWidget(Coordinator());
-    //if a certain phrase can be found on the page, test passes
-    final textFinder = find.text('Injury Count');
-    expect(textFinder, findsOneWidget);
-  });
-  /////// COORDINATOR PAGES TESTS ----  END ///
-
-  ///
-
   //////  EMS WORKER PAGES TEST --- START ////
   testWidgets('EMS Worker Page Test', (tester) async {
     print('EMS Worker Page testing in progress...');
